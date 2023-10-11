616679f89d8839eefa04c6b5ef0211cc632bcc5c
Edward Omondi <ewotieno470@gmail.com>
2023-10-11 11:57:19 +0300

    environment.prod update

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  backendUrl: {
+    base: `http://localhost:8080`,
+    test: `/`,
+    stkpushApi: `/api/push`,
+    stkCallbackApi: `/api/callback`,
+    transactions: `/api/transactions`,
+  },
 };
